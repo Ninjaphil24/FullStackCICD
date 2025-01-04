0505a4dc152623c10610363fe237a2b92037e126
Ninjaphil24 <mphilippos@hotmail.com>
2025-01-04 22:02:28 +0200

    Addendum, 1 test repaired

--- backend/tests/Feature/Auth/RegistrationTest.php
+++ backend/tests/Feature/Auth/RegistrationTest.php
@@ -1,13 +1,16 @@
 <?php
 
-// test('new users can register', function () {
-//     $response = $this->post('/register', [
-//         'name' => 'Test User',
-//         'email' => 'test@example.com',
-//         'password' => 'password',
-//         'password_confirmation' => 'password',
-//     ]);
+test('new users can register', function () {
+    $response = $this->post('api/register', [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'password',
+        'password_confirmation' => 'password',
+    ]);
 
-//     $this->assertAuthenticated();
-//     $response->assertNoContent();
-// });
+    $this->assertAuthenticated();
+    $response->assertStatus(201);
+    $response->assertJson([
+        'message' => 'User registered successfully',
+    ]);
+});
